7708d1f55e4b7e32c864cfa731aa934426bbced9
alexshv <shevchenkostyle@gmail.com>
2017-04-01 21:20:58 +0300

    added susy and changed completely main page

--- src/app.js
+++ src/app.js
@@ -7,18 +7,24 @@ import 'angular-owl-carousel2';
 import 'angular-ui-router';
 import 'angular-ui-bootstrap';
 import 'angular-include-replace';
+// import 'angular-animate';
+// import 'angular-aria';
+// import 'angular-material';
 
 import 'owl-carousel-2/assets/owl.carousel.min.css';
 import 'owl-carousel-2/assets/owl.theme.default.min.css';
 import 'ionicons/dist/scss/ionicons.scss';
+// import 'angular-material/angular-material.min.css';
 import "reset-css/_reset.scss";
+import "susy";
 import '../assets/styles/app.scss';
 
 const app = angular.module('app', [
   'ui.router',
   'ui.bootstrap',
   'angular-include-replace',
-  'angular-owl-carousel-2'
+  'angular-owl-carousel-2',
+  // 'ngMaterial'
 ]);
 
 app.run(['$templateCache', function($templateCache) {
