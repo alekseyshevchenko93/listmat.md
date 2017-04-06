cc075672dcc7e701b643ea90446dfffc52b01df8
alexshv <shevchenkostyle@gmail.com>
2017-04-06 21:05:11 +0300

    removed useless images and added search

--- src/components/main-page/controllers/main-page.controller.js
+++ src/components/main-page/controllers/main-page.controller.js
@@ -2,19 +2,23 @@
 MainPageController.$inject = [];
 
 export default function MainPageController() {
+
   const self = this;
 
   self.sliderItems = [
-    'http://teplitsa-plus.ru/images/stories/tsvetnoj-polikarbonat.jpg',
-    'http://light-vyveski.ru/upload/shop_3/0/9/5/item_95/shop_property_file_95_133.jpg',
-    'http://www.orgaha.ru/data/Image/fotos/orgaha_photo/policorbanat_1.jpg',
-    'http://www.inkton.ru/UserFiles/Image/brands/Inktec/img5361_87592_big.jpg'
+    'http://srbu.ru/images/stroitelnye-materialy/monolitnyj-polikarbonat-svojstva-i-primenenie/monolitnyj-polikarbonat-svojstva-i-primenenie.jpg',
+    'https://www.krov-systema.ru/upload/test/b1dbf6c465a5ea783e8f5be365f84c6b4.jpg',
+    // 'http://www.orgaha.ru/data/Image/fotos/orgaha_photo/policorbanat_1.jpg',
+    // 'https://zorro.by/upload/iblock/716/monolitnyy-polikarbonat-1.jpg',
+    'https://www.krov-systema.ru/upload/test/8e5d49ecca1d21c9fe47a615a24a32dc2.jpg'
   ];
 
   self.sliderProperties = {
     items: 1,
     dots: true,
-    loop: true
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000
   }
 
   console.log('hi from main page');
